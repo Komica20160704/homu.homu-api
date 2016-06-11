949819416edab5ff2cce30b30097abe3f702883e
MickPlaYer <mickgamehome@gmail.com>
2016-06-11 18:17:29 +0800

    Game basic

--- .openshift/action_hooks/pre_stop_.rb
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/ruby -w
-puts "pre_stop_.rb"
-require 'open-uri'
-begin
-  open('http://' + ENV['OPENSHIFT_GEAR_DNS'] + '/game/save')
-rescue Exception => e
-  puts e.message
-end
-puts "pre_stop_.rb end"
