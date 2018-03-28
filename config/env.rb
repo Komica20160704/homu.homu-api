4db77c82c834557ddbca84fc3086dade57fbaef7
Komica20160704 <bluecrazybear@gmail.com>
2018-03-29 02:33:11 +0800

    fix db

--- config/env.rb
+++ config/env.rb
@@ -1,3 +1,3 @@
-require 'sinatra/activerecord'
 ENV['RACK_ENV'] = 'production' if ENV['HOMU_DATABASE_PASSWORD'].present?
+require 'sinatra/activerecord'
 Dir['./models/*.rb'].each { |model| require model }
