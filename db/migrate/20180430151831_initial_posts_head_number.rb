ddef00397500a6e0d0604a91923f65a8de86bc96
Komica20160704 <bluecrazybear@gmail.com>
2018-04-30 23:44:49 +0800

    initial_posts_head_number

---
+++ db/migrate/20180430151831_initial_posts_head_number.rb
@@ -0,0 +1,12 @@
+class InitialPostsHeadNumber < ActiveRecord::Migration[5.1]
+  def up
+    Post.where.not(head_id: nil).includes(:head).find_each do |post|
+      post.head_number = post.head.number
+      post.save!
+    end
+  end
+
+  def down
+    Post.update_all(head_number: nil)
+  end
+end
