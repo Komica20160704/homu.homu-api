ed4082c20bd5297f1bbabde8c7d940005a6a114f
Komica20160704 <bluecrazybear@gmail.com>
2018-04-30 23:06:55 +0800

    fix post scope

--- models/post.rb
+++ models/post.rb
@@ -1,17 +1,18 @@
 class Post < ActiveRecord::Base
-  default_scope { order(:number) }
-  scope :today, -> { where('post_at > ?', Date.today.to_time) }
-  scope :this_week, -> { where('post_at > ?', Date.today.to_time - 7.days) }
-
+  # relations
   belongs_to :head, class_name: 'Post', inverse_of: :bodies
   has_many :bodies, class_name: 'Post', foreign_key: :head_id, inverse_of: :head
-
-  paginates_per 10
-  max_paginates_per 20
-
+  # scopes
+  default_scope { order(:number) }
+  scope :today, -> { where('post_at > ?', Date.today.to_time) }
+  scope :recent, -> { where('post_at > ?', Date.today.to_time - 7.days) }
+  # callbacks
   after_find do |post|
     post.post_at = post.post_at.in_time_zone Time.zone
   end
+  # kaminari
+  paginates_per 10
+  max_paginates_per 20
 
   def is_head?
     head_id.blank?
