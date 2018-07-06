f00224b275df33aebe76198413f1515f99ca9326
Komica20160704 <bluecrazybear@gmail.com>
2018-07-06 23:48:08 +0800

    update for fk nothing just like turtle got hurt!

--- lib/hosts/const.rb
+++ lib/hosts/const.rb
@@ -3,8 +3,8 @@
 module Hosts
   module Const
     KOMICA_URL = 'https://web.komica.org/'
-    HOMU_INDEX = 'https://ram.komica2.net/00/'
-    HOMU_IMAGE = 'https://ram.komica2.net/00/'
+    HOMU_INDEX = 'https://sora.komica.org/00/'
+    HOMU_IMAGE = 'https://sora.komica.org/00/'
     HOMU_API_URL = 'https://www.homu-api.com/'
   end
 end
