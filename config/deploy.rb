bd2ab3d907f1223c4630f45c03124104517486c4
Komica20160704 <bluecrazybear@gmail.com>
2018-10-07 01:21:44 +0800

    Add thin:hot_reload to cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,13 +13,29 @@ set :keep_releases, 2
 namespace :thin do
   commands = %i[start stop restart]
 
+  def thin_server(command, config_file)
+    execute :bundle, 'exec', :thin, command, "--config #{config_file}"
+  end
+
   commands.each do |command|
     task command do
       on roles :app do
         within current_path do
-          config_file = 'config/thin.yml'
-          execute :bundle, 'exec', :thin, command, "--config #{config_file}"
+          thin_server(command, 'config/thin.yml')
         end
+      end
+    end
+  end
+
+  task :hot_reload do
+    on roles :app do
+      within current_path do
+        config_file = 'config/thin.yml'
+        hot_reload_config_file = 'config/thin_hot_reload.yml'
+        thin_server(:start, hot_reload_config_file)
+        thin_server(:stop, config_file)
+        thin_server(:start, config_file)
+        thin_server(:stop, hot_reload_config_file)
       end
     end
   end
