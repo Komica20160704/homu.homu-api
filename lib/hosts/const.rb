04273caa7d22708059de3406de2e3c8513eaeb48
Komica20160704 <bluecrazybear@gmail.com>
2022-09-26 13:53:59 +0800

    fix image domain

--- lib/hosts/const.rb
+++ lib/hosts/const.rb
@@ -4,7 +4,7 @@ module Hosts
   module Const
     KOMICA_URL = 'https://web.komica.org/'
     HOMU_INDEX = 'https://gaia.komica.org/00/'
-    HOMU_IMAGE = 'https://ram.komica2.net/00'
+    HOMU_IMAGE = 'https://gaia.komica.org/00'
     HOMU_API_URL = 'https://www.homu-api.com/'
   end
 end
